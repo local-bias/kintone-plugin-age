1a229f8bcd95d22be75f74091865a7231745d317
Ribbit <echo07200503@gmail.com>
2021-09-16 10:13:50 +0900

    バンドル設定の変更と、それに伴って不要となったライブラリの削除

--- webpack.common.js
+++ webpack.common.js
@@ -1,46 +1,6 @@
 const path = require('path');
-const KintonePlugin = require('@kintone/webpack-plugin-kintone-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /.ts?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-typescript'],
-          },
-        },
-      },
-      {
-        test: /.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/react', '@babel/preset-typescript'],
-          },
-        },
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json'],
-  },
-
   entry: {
     desktop: './src/desktop/index.ts',
     config: './src/config/index.ts',
@@ -49,11 +9,24 @@ module.exports = {
     path: path.resolve(__dirname, 'plugin', 'js'),
     filename: '[name].js',
   },
-  plugins: [
-    new KintonePlugin({
-      manifestJSONPath: './plugin/manifest.json',
-      privateKeyPath: './private.ppk',
-      pluginZipPath: './dist/plugin.zip',
-    }),
-  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', 'json'],
+    alias: {
+      '@common': path.resolve(__dirname, 'src/common'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          },
+        },
+      },
+    ],
+  },
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,21 @@
-const { merge } = require("webpack-merge");
-const common = require("./webpack.common.js");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const KintonePlugin = require('@kintone/webpack-plugin-kintone-plugin');
 
 module.exports = merge(common, {
-  mode: "production",
+  mode: 'development',
+
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
+  plugins: [
+    new KintonePlugin({
+      manifestJSONPath: './plugin/manifest.json',
+      privateKeyPath: './private.ppk',
+      pluginZipPath: './dist/plugin-dev.zip',
+    }),
+  ],
 });
