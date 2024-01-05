8b128168554b0af186dd23ef6b15ff9876ebf0cd
Ribbit <echo07200503@gmail.com>
2024-01-05 10:52:01 +0900

    ✨ Add event manager module

---
+++ src/lib/event-manager.ts
@@ -0,0 +1,16 @@
+import { KintoneEventManager } from '@konomi-app/kintone-utilities';
+import { PLUGIN_NAME } from './statics';
+
+export const manager = new KintoneEventManager({
+  errorHandler: (error, props) => {
+    const { event } = props;
+    event.error = `プラグイン「${PLUGIN_NAME}」の処理内でエラーが発生しました。`;
+    console.error('エラー', error);
+    return event;
+  },
+  logPrefix: `[${PLUGIN_NAME}] `,
+  logDisabled: process.env.NODE_ENV === 'production',
+});
+
+process.env.NODE_ENV === 'development' &&
+  console.info('[plugin] Event listener has been initialized');
