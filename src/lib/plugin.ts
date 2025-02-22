37b6fb32aa8d198775fa03b2d8857fe71fa45e94
Ribbit <echo07200503@gmail.com>
2025-02-22 10:20:10 +0900

    ♻ プラグイン設定情報の型をzodで再定義

--- src/lib/plugin.ts
+++ src/lib/plugin.ts
@@ -1,9 +1,10 @@
+import { AnyPluginConfig, PluginCondition, PluginConfig } from '@/schema/plugin-config';
 import { restoreStorage } from '@konomi-app/kintone-utilities';
 import { produce } from 'immer';
-import { PLUGIN_ID } from './global';
 import { nanoid } from 'nanoid';
+import { PLUGIN_ID } from './global';
 
-export const getNewCondition = (): Plugin.Condition => ({
+export const getNewCondition = (): PluginCondition => ({
   id: nanoid(),
   srcFieldCode: '',
   dstFieldCode: '',
@@ -13,7 +14,7 @@ export const getNewCondition = (): Plugin.Condition => ({
 /**
  * プラグインの設定情報のひな形を返却します
  */
-export const createConfig = (): Plugin.Config => ({
+export const createConfig = (): PluginConfig => ({
   version: 3,
   conditions: [getNewCondition()],
 });
@@ -23,7 +24,7 @@ export const createConfig = (): Plugin.Config => ({
  * @param anyConfig 保存されている設定情報
  * @returns 新しいバージョンの設定情報
  */
-export const migrateConfig = (anyConfig: Plugin.AnyConfig): Plugin.Config => {
+export const migrateConfig = (anyConfig: AnyPluginConfig): PluginConfig => {
   const { version } = anyConfig;
   switch (version) {
     case undefined:
@@ -50,17 +51,17 @@ export const migrateConfig = (anyConfig: Plugin.AnyConfig): Plugin.Config => {
 /**
  * プラグインの設定情報を復元します
  */
-export const restorePluginConfig = (): Plugin.Config => {
-  const config = restoreStorage<Plugin.AnyConfig>(PLUGIN_ID) ?? createConfig();
+export const restorePluginConfig = (): PluginConfig => {
+  const config = restoreStorage<AnyPluginConfig>(PLUGIN_ID) ?? createConfig();
   return migrateConfig(config);
 };
 
-export const getUpdatedStorage = <T extends keyof Plugin.Condition>(
-  storage: Plugin.Config,
+export const getUpdatedStorage = <T extends keyof PluginCondition>(
+  storage: PluginConfig,
   props: {
     conditionIndex: number;
     key: T;
-    value: Plugin.Condition[T];
+    value: PluginCondition[T];
   }
 ) => {
   const { conditionIndex, key, value } = props;
@@ -69,14 +70,14 @@ export const getUpdatedStorage = <T extends keyof Plugin.Condition>(
   });
 };
 
-export const getConditionField = <T extends keyof Plugin.Condition>(
-  storage: Plugin.Config,
+export const getConditionField = <T extends keyof PluginCondition>(
+  storage: PluginConfig,
   props: {
     conditionIndex: number;
     key: T;
-    defaultValue: NonNullable<Plugin.Condition[T]>;
+    defaultValue: NonNullable<PluginCondition[T]>;
   }
-): NonNullable<Plugin.Condition[T]> => {
+): NonNullable<PluginCondition[T]> => {
   const { conditionIndex, key, defaultValue } = props;
   if (!storage.conditions[conditionIndex]) {
     return defaultValue;
